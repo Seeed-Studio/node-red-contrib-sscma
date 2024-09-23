8894a60949cb4a80abc9201d2eec9d01d116daa3
LynnL4 <lht856@foxmail.com>
2024-09-23 17:25:08 +0800

    feat: add count display to preview node

--- nodes/preview.js
+++ nodes/preview.js
@@ -1,34 +1,42 @@
-module.exports = function(RED) {
-   
+module.exports = function (RED) {
+
     function PreviewNode(config) {
         RED.nodes.createNode(this, config);
         var node = this;
-        this.active     = (config.active === null || typeof config.active === "undefined") || config.active;
-        
+        this.active = (config.active === null || typeof config.active === "undefined") || config.active;
+
         function handleError(err, msg, statusText) {
-            node.status({ fill:"red", shape:"dot", text:statusText });
+            node.status({ fill: "red", shape: "dot", text: statusText });
             node.error(err, msg);
         }
-        node.on("input", function(msg) {
+        node.on("input", function (msg) {
             if (this.active !== true) { return; }
-            RED.comms.publish("image", { id:node.id, data:msg.payload.data });
+            RED.comms.publish("image", { id: node.id, data: msg.payload.data });
+            if (msg.payload.data.counts) {
+                const countA = msg.payload.data.counts[0];
+                const countB = msg.payload.data.counts[1];
+                const countAB = msg.payload.data.counts[2];
+                const countBA = msg.payload.data.counts[3];
+                let textContent = `A: ${countA} B: ${countB} A->B: ${countAB} B->A: ${countBA}`
+                node.status({ fill: "blue", shape: "dot", text: textContent });
+            }
         });
 
-        node.on("close", function() {
-            RED.comms.publish("image", { id:this.id });
+        node.on("close", function () {
+            RED.comms.publish("image", { id: this.id });
             node.status({});
         });
     }
     RED.nodes.registerType("preview", PreviewNode);
-    
+
     // Via the button on the node (in the FLOW EDITOR), the image pushing can be enabled or disabled
-    RED.httpAdmin.post("/image-output/:id/:state", RED.auth.needsPermission("image-output.write"), function(req,res) {
+    RED.httpAdmin.post("/image-output/:id/:state", RED.auth.needsPermission("image-output.write"), function (req, res) {
         var state = req.params.state;
         var node = RED.nodes.getNode(req.params.id);
-        
-        if(node === null || typeof node === "undefined") {
+
+        if (node === null || typeof node === "undefined") {
             res.sendStatus(404);
-            return;  
+            return;
         }
         if (state === "enable") {
             node.active = true;
