f364a96ef9023b607586e27afc9563eb15335234
LynnL4 <lht856@foxmail.com>
2024-10-23 15:23:42 +0800

    feat: model node support light control

--- nodes/camera.js
+++ nodes/camera.js
@@ -7,7 +7,8 @@ module.exports = function (RED) {
         node.client = RED.nodes.getNode(n.client);
         node.config = {
             option: n.option || 0,
-            preview: false
+            preview: false,
+            light: n.active ? 1 : 0
         }
 
         // if connect to preview, set preview to true
@@ -38,6 +39,31 @@ module.exports = function (RED) {
         });
 
     }
+
+    // Via the button on the node (in the FLOW EDITOR), the image pushing can be enabled or disabled
+    RED.httpAdmin.post("/camera/:id/:state", RED.auth.needsPermission('camera.write'), function (req, res) {
+        var id = req.params.id;
+        var state = req.params.state;
+        console.log(id, state);
+        var node = RED.nodes.getNode(id);
+        if (node === null) {
+            res.send(404);
+            return;
+        }
+        if (state === "on") {
+            console.log("on");
+            node.config.light = 1;
+            node.client.request(node.id, "light", node.config.light);
+            res.send("on");
+        } else if (state === "off") {
+            console.log("off");
+            node.config.light = 0;
+            node.client.request(node.id, "light", node.config.light);
+            res.send("off");
+        } else {
+            res.send(200);
+        }
+    });
 
     RED.nodes.registerType("camera", CameraNode);
 }
