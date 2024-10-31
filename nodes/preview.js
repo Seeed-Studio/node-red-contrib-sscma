e6859373fe2d6a13fc7bbf635f262760db4d3562
LynnL4 <lht856@foxmail.com>
2024-10-31 16:15:00 +0800

    fix:eoOptimize the keep-alive judgment mechanis of preview node

--- nodes/preview.js
+++ nodes/preview.js
@@ -3,20 +3,20 @@ module.exports = function (RED) {
     function PreviewNode(config) {
         RED.nodes.createNode(this, config);
         var node = this;
-        this.enabled = true;
-        this.alive = 0;
+        this.alive = true;
         this.active = (config.active === null || typeof config.active === "undefined") || config.active;
 
+        node.timeout = setTimeout(function () {
+            node.alive = false;
+            node.timeout = null;
+            if (global.gc) {
+                global.gc();
+            }
+        }, 3000);
+
         node.on("input", function (msg) {
-            if (this.active !== true || this.enabled !== true) { return; }
+            if (this.active !== true || this.alive !== true) { return; }
             if (msg.payload.name === "invoke" || msg.payload.name === "sample") {
-                if (msg.payload.data.count % 60 === 0) {
-                    node.alive = node.alive + 1;
-                    if (node.alive === 3) {
-                        node.enabled = false;
-                        node.alive = 0;
-                    }
-                }
                 RED.comms.publish("preview", { id: node.id, data: msg.payload.data });
                 if (msg.payload.data.counts) {
                     const countA = msg.payload.data.counts[0];
@@ -26,11 +26,24 @@ module.exports = function (RED) {
                     let textContent = `A: ${countA} B: ${countB} A->B: ${countAB} B->A: ${countBA}`
                     node.status({ fill: "blue", shape: "dot", text: textContent });
                 }
+                if (node.timeout === null) {
+                    node.timeout = setTimeout(function () {
+                        node.alive = false;
+                        node.timeout = null;
+                        if (global.gc) {
+                            global.gc();
+                        }
+                    }, 3000);
+                }
             }
         });
 
         node.on("close", function () {
             RED.comms.publish("preview", { id: this.id });
+            if (this.timeout !== null) {
+                clearTimeout(this.timeout);
+                this.timeout = null;
+            }
             node.status({});
         });
     }
@@ -48,16 +61,16 @@ module.exports = function (RED) {
                     return;
                 }
                 if (n.type === "preview") {
-                    if (state === "start") {
-                        node.enabled = true;
-                        node.alive = 0;
-                    }
-                    else if (state === "stop") {
-                        node.enabled = false;
-                        node.alive = 0;
+                    if (state === "alive") {
+                        node.alive = true;
+                        if (node.timeout !== null) {
+                            clearTimeout(node.timeout);
+                            node.timeout = null;
+                        }
                     }
                 }
             })
+            res.send('alive');
             return;
         }
         if (state === "enable") {
@@ -68,14 +81,13 @@ module.exports = function (RED) {
             node.active = false;
             res.send('deactivated');
         }
-        else if (state === "start") {
-            node.enabled = true;
-            node.alive = 0;
-            res.send('started');
-        } else if (state === "stop") {
-            node.enabled = false;
-            node.alive = 0;
-            res.send('stopped');
+        else if (state === "alive") {
+            node.alive = true;
+            if (node.interval !== null) {
+                clearInterval(node.interval);
+                node.interval = null;
+            }
+            res.send('alive');
         }
         else {
             res.sendStatus(404);
