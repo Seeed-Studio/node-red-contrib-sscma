7cb571449aed24e1e90497b27926c6aff73e9437
wujingyue <wutiange20@qq.com>
2024-12-26 11:44:35 +0800

    chore: switch from single quotes to double quotes

--- .prettierrc.js
+++ .prettierrc.js
@@ -3,9 +3,9 @@ module.exports = {
     tabWidth: 4, //缩进长度
     useTabs: false, //使用空格代替tab缩进
     semi: true, //句末使用分号
-    singleQuote: true, //使用单引号
+    singleQuote: false, //使用单引号
     quoteProps: 'as-needed', //仅在必需时为对象的key添加引号
-    jsxSingleQuote: true, // jsx中使用单引号
+    jsxSingleQuote: false, // jsx中使用单引号
     trailingComma: 'all', //多行时尽可能打印尾随逗号
     bracketSpacing: true, //在对象前后添加空格-eg: { foo: bar }
     jsxBracketSameLine: true, //多属性html标签的‘>’折行放置
